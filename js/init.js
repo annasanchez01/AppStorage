46b83c7f66f07380f3b3255dcabb3ed1cd1c33a0
Ana Paula <71850599+annasanchez01@users.noreply.github.com>
2021-11-28 22:55:45 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyC85s-6cJBQ0Nn0SlFrxpIXnpRPa4XP5cU",
+  authDomain: "app-web-ecbb7.firebaseapp.com",
+  projectId: "app-web-ecbb7",
+  storageBucket: "app-web-ecbb7.appspot.com",
+  messagingSenderId: "106166255923",
+  appId: "1:106166255923:web:4a531cdc66aa1d544d3e46",
+  measurementId: "G-TMRTZQL0XZ"
+});
